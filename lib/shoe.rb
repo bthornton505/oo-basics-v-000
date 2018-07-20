e154e97de3acf212c4817fe44f494e20d31e96fc
Brenden Thornton <thorntonbrenden@gmail.com>
2018-07-20 14:00:04 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,8 @@
 class Shoe 
-  attr_accessor :brand, :color, :size, :material
+  attr_accessor :brand, :color, :size, :material, :condition
   
   def initialize(brand)
     @brand = brand 
-  end 
-  
-  def condition
   end 
   
 end 
